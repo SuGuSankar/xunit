88c727a8474d096eaddb7c64ff116a1f538b563f
Sugumar Sankar <sugumar05sugu@gmail.com>
2023-04-24 19:06:14 +0530

    xunit commit

---
+++ Xunit/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Xunit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("Xunit First Test");
+        }
+    }
+}
